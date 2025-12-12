3b20c00e9edd4e69c707fa0c433c1f7ff4cb7be3
artiordex <artiordex@gmail.com>
2025-12-12 21:45:42 +0900

    Git Action deploy test

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,13 +3,8 @@ import { resolve } from "path";
 
 export default defineConfig(({ command }) => {
   const isProd = command === "build";
-
-  // GitHub Pages (repo-name 방식)
-  const base = isProd ? "/artiordex.github.io/" : "/";
-
   return {
-    base,
-
+    base: "/",
     root: ".",
     publicDir: "assets",
 
